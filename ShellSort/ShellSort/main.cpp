b57574506f62eb3e304c31cf7aeb924713b31434
Chaomin702 <chaomin702@gmail.com>
2016-01-28 22:19:37 +0800

    ShellSort

--- ShellSort/ShellSort/main.cpp
+++ ShellSort/ShellSort/main.cpp
@@ -6,12 +6,19 @@ using namespace std;
 void shellSort(vector<int> &A){
 	for (int gap = A.size() / 2; gap > 0; gap = gap / 2){
 		for (int k = gap + 1; k < A.size(); k++){
-
+			for (int j = k - gap; j >= 0; j = j - gap){
+				if (A[j + gap]<A[j]){
+					swap(A[j + gap], A[j]);
+				}else{ 
+					break;
+				}
+			}
 		}
 	}
 }
 
 int main(void){
-
+	vector<int> A = { 6, 5, 4, 3, 2, 1, 0 };
+	shellSort(A);
 	return 0;
 }
